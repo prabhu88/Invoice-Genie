71491022bb62e40cbd62ac2b8f3cb85901558f7c
prabhu88 <s.prabhu.mtech88@gmail.com>
2023-01-28 11:50:06 +0530

    commit

--- app/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-// import UIReducer from './UIReducer';
-// import FormReducer from './FormReducer';
-// import InvoicesReducer from './InvoicesReducer';
-// import ContactsReducer from './ContactsReducer';
-// import SettingsReducer from './SettingsReducer';
-
-export default combineReducers({});
